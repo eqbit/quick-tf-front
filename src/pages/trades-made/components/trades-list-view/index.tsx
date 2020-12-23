4af64601e395570f094a1bd1e502820c486abfc4
eqbit <eqbits@gmail.com>
2020-12-23 12:02:17 +0500

    link from past trades to unusual detail

--- src/pages/trades-made/components/trades-list-view/index.tsx
+++ src/pages/trades-made/components/trades-list-view/index.tsx
@@ -2,6 +2,7 @@ import React, { memo } from 'react';
 import { RegisteredTrade } from '../../../../api/requests/trades/types';
 import classNamesBind from 'classnames/bind';
 import styles from './index.module.scss';
+import { Link } from 'react-router5';
 
 const CLASS_NAME = 'trades-made';
 const cn = classNamesBind.bind(styles);
@@ -17,44 +18,50 @@ const Component = ({ tradesMade }: Props) => {
         const tradeDirection = trade.direction ? 'Sale' : 'Buy';
 
         return (
-          <li key={trade.id} className={cn(`${CLASS_NAME}__row`)}>
-            <div
-              className={cn(
-                `${CLASS_NAME}__cell`,
-                `${CLASS_NAME}__cell--${tradeDirection.toLowerCase()}`
-              )}
+          <li key={trade.id}>
+            <Link
+              className={cn(`${CLASS_NAME}__row`)}
+              routeName="unusuals.hat.hat-detail"
+              routeParams={{ name: trade.name, effect: trade.effect }}
             >
-              {`${tradeDirection}`}
-            </div>
+              <div
+                className={cn(
+                  `${CLASS_NAME}__cell`,
+                  `${CLASS_NAME}__cell--${tradeDirection.toLowerCase()}`
+                )}
+              >
+                {`${tradeDirection}`}
+              </div>
 
-            <div
-              className={cn(
-                `${CLASS_NAME}__cell`,
-                `${CLASS_NAME}__cell--time`
-              )}
-            >
-              {`${new Date(Number(trade.timestamp)).toLocaleDateString('ru')}`}
-            </div>
+              <div
+                className={cn(
+                  `${CLASS_NAME}__cell`,
+                  `${CLASS_NAME}__cell--time`
+                )}
+              >
+                {`${new Date(Number(trade.timestamp)).toLocaleDateString('ru')}`}
+              </div>
 
-            <div
-              className={cn(
-                `${CLASS_NAME}__cell`,
-                `${CLASS_NAME}__cell--item-name`,
-                `${CLASS_NAME}__cell--${trade.quality.toLowerCase()}`,
-              )}
-            >
-              {`${trade.effect !== 'none' ? trade.effect : trade.quality} ${trade.name}`}
-            </div>
+              <div
+                className={cn(
+                  `${CLASS_NAME}__cell`,
+                  `${CLASS_NAME}__cell--item-name`,
+                  `${CLASS_NAME}__cell--${trade.quality.toLowerCase()}`,
+                )}
+              >
+                {`${trade.effect !== 'none' ? trade.effect : trade.quality} ${trade.name}`}
+              </div>
 
-            <div
-              className={cn(
-                `${CLASS_NAME}__cell`,
-                `${CLASS_NAME}__cell--currency`
-              )}
-            >
-              {Boolean(trade.keys) && <span>{`${trade.keys} keys`}</span>}
-              {Boolean(trade.metal) && <span>{`${trade.metal} ref`}</span>}
-            </div>
+              <div
+                className={cn(
+                  `${CLASS_NAME}__cell`,
+                  `${CLASS_NAME}__cell--currency`
+                )}
+              >
+                {Boolean(trade.keys) && <span>{`${trade.keys} keys`}</span>}
+                {Boolean(trade.metal) && <span>{`${trade.metal} ref`}</span>}
+              </div>
+            </Link>
           </li>
         )
       })}
